75fcce339841baad63222b88007ba391ce65bc7e
Diego <alejo.canda777@gmail.com>
2026-08-12 15:42:36 -0500

    estructura

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,7 @@
-# Entrypoint del backend
+from fastapi import FastAPI
+
+app = FastAPI(title="API Turnero Digital")
+
+@app.get("/")
+def read_root():
+    return {"mensaje": "¡El backend en Python está vivo y funcionando en el puerto 8100!"}# Entrypoint del backend
